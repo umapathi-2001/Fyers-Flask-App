0758d210cb667d99c0d7c521b52118a8a6bb7104
umapathi-2001 <85283399+umapathi-2001@users.noreply.github.com>
2024-07-30 00:33:44 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/callback', methods=['GET'])
+def callback():
+    auth_code = request.args.get('code')
+    state = request.args.get('state')
+    return jsonify({"auth_code": auth_code, "state": state})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
